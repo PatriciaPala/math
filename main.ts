58568c73034e02baa5acbded2217bb5bad5cb375
PatriciaPala <101339915+PatriciaPala@users.noreply.github.com>
2022-04-08 09:32:12 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    A = number + 9
+    B = number - -9
+    C = number ** 2
+    D = number / 3
+    E = number / 4
+    F = Math.sqrt(number)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(A)
+    basic.showNumber(B)
+    basic.showNumber(C)
+})
+let F = 0
+let E = 0
+let D = 0
+let C = 0
+let B = 0
+let A = 0
+let number = 0
+number = 8
+basic.forever(function () {
+	
+})
